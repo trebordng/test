2d1b62b0157c94ab88521e17dda1755991b70be5
trebordng <96870545+trebordng@users.noreply.github.com>
2023-05-19 17:10:45 +1000

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import imagemin from "imagemin";
 import imageminJpegtran from "imagemin-jpegtran";
 import getBase64ImageUrl from "@/utils/generateBlurPlaceholder";
 import home from "../public/images/home.jpg";
-import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ images }: { images: any }) {
@@ -15,14 +14,18 @@ export default function Home({ images }: { images: any }) {
     >
       <Image
         alt="Next.js Conf photo"
+        className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+        style={{ transform: "translate3d(0, 0, 0)" }}
         placeholder="blur"
         blurDataURL={images.blurDataURL}
         src={images.src}
         width={720}
         height={480}
-        
+        sizes="(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw"
       />
-       
     </main>
   );
 }
